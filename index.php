5e1e7929840f70c6acf1f57aafccf4bb39bee521
wulandarikaranamu02@gmail.com <wulandarikaranamu02@gmail.com>
2023-10-23 01:18:52 -0700

    update bye merry

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+include_once 'koneksi.php';
+include_once 'models/Kategori.php';
+include_once 'index.php';
+
+?>
+
+<div>
+    <div class="continer-fluid px-4">
+        <!-- <h1>Selamat Datang di Halaman Admin</h1> -->
+
+        <?php 
+        error_reporting(0); //ini digunakan untuk menyembunyikan error
+        //membuat logik sederhana untuk mengarahkan url ke file yang
+        //mempunyai extension.php
+        //routing => file yang mengarahkan url
+
+        /* ini juga bisa digunakan untuk menutupi error
+        $url = !isset($_GET['url']) ? 'dashboard' : $_GET['url'];
+        */
+
+        $url = $_GET['url'];
+        if($url == 'dashboard'){
+            include_once 'dashboard.php';
+        }else if (!empty($url)){
+            include_once ''.$url.'.php';
+        }else{
+            include_once 'dashboard.php';
+        }
+        
+        ?>
+    </div>
+</div>
+
+<?php 
+    include_once 'bottom.php';
+
+?>
---
+++ models/Kategori.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Kategori{
+    private $koneksi;
+    public function __construct(){
+        global $dbh;
+        $this->koneksi = $dbh;
+    }
+
+    //mengambil dan melihat tabel jenis_produk
+    public function dataKategori(){
+    $sql = "SELECT * FROM kategori";
+    //menggunakan mekanisme prepare statement PDO
+    $ps = $this->koneksi->prepare($sql);
+    $ps->execute();
+    $rs = $ps->fetchAll();
+    return $rs;
+}
+}
+
+?>
