def37d536ed950d4bc99668fc0d6f3ba453c5ac6
agusabdulmh <agusmanan17171@gmail.com>
2023-10-27 00:00:49 +0700

    crud lihat detail kategori & edit kategori

--- admin/kategori.php
+++ admin/kategori.php
@@ -12,8 +12,7 @@ $data_kategori = $model->dataKategori();
     <nav>
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item">Tables</li>
-        <li class="breadcrumb-item active">Kategori Gejla</li>
+        <li class="breadcrumb-item active">Kategori Gejala</li>
         <li class="breadcrumb-item active">Kategori</li>
       </ol>
     </nav>
@@ -30,33 +29,40 @@ $data_kategori = $model->dataKategori();
 
             <!-- Table with stripped rows -->
             <table class="table datatable">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Kategori</th>
-                  <!-- <th scope="col">Deskripsi</th>
-                    <th scope="col">nama</th> -->
+             <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Kategori</th>
+      <th scope="col">Actions</th> <!-- Add a new column for the button -->
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $no = 1;
+    foreach ($data_kategori as $kategori) {
+    ?>
+      <tr>
+        <th scope="row"><?= $no ?></th>
+        <td><?= $kategori['nama_kategori'] ?></td>
+        <td>
+          <form>
+            <a href="index.php?url=kategori_detail&id=<?=$kategori['id']?>">
+              <button type="button" class="btn btn-outline-primary">Detail</button>
+            </a>
+            <a href="index.php?url=kategori_edit&id=<?=$kategori['id']?>">
+              <button type="button" class="btn btn-primary">Edit</button>
+            </a>
+          </form>
+        </td>
+      
+      </tr>
+    <?php
+      $no++;
+    }
+    ?>
+  </tbody>
+</table>
 
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-
-                $no = 1;
-                foreach ($data_kategori as $kategori) {
-                ?>
-                  <tr>
-                    <th scope="row"><?= $no ?></th>
-                    <td><?= $kategori['nama_kategori'] ?></td>
-                    <!-- <td><?= $kategori['deskripsi'] ?></td>
-                    <td><?= $kategori['nama_user'] ?></td> -->
-                  </tr>
-                <?php
-                  $no++;
-                }
-                ?>
-              </tbody>
-            </table>
             <!-- End Table with stripped rows -->
 
           </div>
--- admin/models/Kategori.php
+++ admin/models/Kategori.php
@@ -10,12 +10,32 @@ class Kategori{
     //mengambil dan melihat tabel jenis_produk
     public function dataKategori(){
     $sql = "SELECT * FROM kategori";
+    
     //menggunakan mekanisme prepare statement PDO
     $ps = $this->koneksi->prepare($sql);
     $ps->execute();
     $rs = $ps->fetchAll();
     return $rs;
 }
+
+    //menampilkan detail nama_kategori
+    public function detailKategori($id){
+        $sql = "SELECT * FROM kategori WHERE id = :id";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute(['id' => $id]);
+        $rs = $ps->fetch();
+        return $rs;
+    }
+
+    //edit nama_kategori
+    public function editKategori($id, $nama_kategori){
+        $sql = "UPDATE kategori SET nama_kategori = :nama_kategori WHERE id = :id";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute(['id' => $id, 'nama_kategori' => $nama_kategori]);
+        $row = $ps->rowCount();
+        return $row;
+    }
+
 }
 
 ?>
