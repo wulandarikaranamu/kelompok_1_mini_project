bd71134b1218d10b9155206c3f12676340f383bd
agusabdulmh <agusmanan17171@gmail.com>
2023-10-31 23:25:24 +0700

    update navbar/menu

--- register_process.php
+++ register_process.php
@@ -17,17 +17,17 @@ $umur = $_POST['umur'];
 $jenis_kelamin = $_POST['jenis_kelamin'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$role = "user"; 
+$role = "admin"; 
 $hashedPassword = sha1(md5(sha1($password)));
 
 // Simpan data ke tabel User
 $query = "INSERT INTO User (nama, umur, jenis_kelamin, email, password, role) VALUES ('$nama', '$umur', '$jenis_kelamin', '$email', '$hashedPassword', '$role')";
 
 if ($conn->query($query) === TRUE) {
-    // Pendaftaran berhasil, arahkan ke halaman login
-    header("Location: index.php");
+    // Pendaftaran berhasil, tampilkan popup
+    echo '<script>alert("Registrasi berhasil. Silakan login.");</script>';
+    echo '<script>window.location.href = "index.php";</script>';
 } else {
     echo "Error: " . $conn->error;
 }
-
 $conn->close();
